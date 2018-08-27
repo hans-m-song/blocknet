131815540569f9a37f7991665c14cc3a4df395fa
Craig Harvey <craigharvey@uqconnect.edu.au>
2018-08-27 21:30:59 +1000

    advanced 3d graphics

---
+++ promo-website/js/box.js
@@ -0,0 +1,30 @@
+var camera, scene, renderer;
+			var mesh;
+			init();
+			animate();
+			function init() {
+				camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+				camera.position.z = 400;
+				scene = new THREE.Scene();
+				var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
+				var material = new THREE.MeshBasicMaterial();
+				mesh = new THREE.Mesh( geometry, material );
+				scene.add( mesh );
+				renderer = new THREE.WebGLRenderer( { antialias: true } );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				document.body.appendChild( renderer.domElement );
+				//
+				window.addEventListener( 'resize', onWindowResize, false );
+			}
+			function onWindowResize() {
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+				renderer.setSize( window.innerWidth, window.innerHeight );
+			}
+			function animate() {
+				requestAnimationFrame( animate );
+				mesh.rotation.x += 0.005;
+				mesh.rotation.y += 0.01;
+				renderer.render( scene, camera );
+			}
